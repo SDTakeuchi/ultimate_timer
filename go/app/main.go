77ee311d74f05ba731e977b775c51ad182be01ac
SDTakeuchi <kuranku191952996@gmail.com>
2022-04-25 20:01:48 +0900

    go dockerfile updated

--- go/app/main.go
+++ go/app/main.go
@@ -7,20 +7,19 @@ import (
 	"ultimate_timer/services"
 	"ultimate_timer/usecase"
 
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-
 func main() {
-    presetRepository := infra.NewPresetRepository(config.NewDB(), config.NewRedis())
-    presetUsecase := usecase.NewPresetUsecase(presetRepository)
-    presetHandler := handler.NewPresetHandler(presetUsecase)
+	presetRepository := infra.NewPresetRepository(config.NewDB(), config.NewRedis())
+	presetUsecase := usecase.NewPresetUsecase(presetRepository)
+	presetHandler := handler.NewPresetHandler(presetUsecase)
 
-    e := echo.New()
+	e := echo.New()
 	e.Use(services.Logger)
 	e.Use(middleware.Recover())
-    handler.InitRouting(e, presetHandler)
-    e.Logger.Fatal(e.Start(":8080"))
+	handler.InitRouting(e, presetHandler)
+	e.Logger.Fatal(e.Start(":8080"))
 }
